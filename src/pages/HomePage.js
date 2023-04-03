0e40297f9ccee2a3a231a33d1b9d0b21a9d2a909
AyusDas <45256705+AyusDas@users.noreply.github.com>
2023-04-03 23:33:25 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
 import Header from '../components/Header';
-import { FcSearch } from "react-icons/fc";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdLocationOn } from "react-icons/md";
+import { GiPillDrop, GiBrain, GiChemicalDrop } from "react-icons/gi";
+import { RiWomenLine } from "react-icons/ri";
+import { FaHeartbeat, FaTeeth } from "react-icons/fa";
+import { TbPhysotherapist, TbBone } from "react-icons/tb";
+import { FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
@@ -38,7 +44,7 @@ const HomePage = () => {
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
-            items: 4,
+            items: 5,
             slidesToSlide: 1
         }
     };
@@ -56,24 +62,26 @@ const HomePage = () => {
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
-            items: 2,
+            items: 1.3,
             slidesToSlide: 1
         }
     };
-
-    const carouselStyle = "bg-blue-600 h-44 border border-blue-600 rounded-md mx-3 text-center text-white font-semibold text-xl py-5"
-    const carStyle2 = "border border-black rounded-md shadow-lg px-2 mx-2"
+    
+    const carStyle2 = "rounded-md px-2 mx-2 flex flex-1 items-center justify-center h-full"
 
     return (
         <div>
             <Header/>
 
-            <div className='flex flex-1 justify-between items-center'>
+            <div className='flex flex-1 justify-between items-center bg-white'>
                 <div className='px-5 py-5'>
                     <div className='text-xl'>Find Your</div>
-                    <div className='text-2xl font-semibold'>Specialist</div>
+                    <div className='text-3xl font-semibold'>Specialist</div>
                 </div>
-                <div className='text-3xl px-5'><Link to="/search"><FcSearch/></Link></div>
+                <div className='text-3xl px-5 flex gap-3'>
+                    <Link to="/search"><AiOutlineSearch/></Link>
+                    <Link><MdLocationOn/></Link>
+                </div>
             </div>
 
             <Carousel
@@ -90,23 +98,23 @@ const HomePage = () => {
                 dotListClass="custom-dot-list-style"
                 itemClass="carousel-item-padding-40-px">
 
-                <div className={carouselStyle}>
-                    <p>Doctor 1</p>
+                <div className='flex flex-col items-center rounded-sm'>
+                    <img className='h-52 w-full' src={require("../assets/ad1.png")}/>
                 </div>
-                <div className={carouselStyle}>
-                    <p>Doctor 2</p>
+                <div className='flex flex-col items-center rounded-sm'>
+                    <img className='h-52 w-full' src={require("../assets/ad2.png")}/>
                 </div>
-                <div className={carouselStyle}>
-                    <p>Doctor 3</p>
+                <div className='flex flex-col items-center rounded-sm'>
+                    <img className='h-52 w-full' src={require("../assets/ad3.png")}/>
                 </div>
-                <div className={carouselStyle}>
-                    <p>Doctor 4</p>
+                <div className='flex flex-col items-center rounded-sm'>
+                    <img className='h-52 w-full' src={require("../assets/ad4.png")}/>
                 </div>
             </Carousel>
 
-            <div className='py-5 px-5 flex flex-1 justify-between items-center'>
+            <div className='mt-5 py-5 px-5 flex flex-1 justify-between items-center'>
                 <div className='text-xl font-semibold'>Categories</div>
-                <div>see all</div>
+                <div><Link to='/doctors'>see all</Link></div>
             </div>
 
             <Carousel
@@ -121,37 +129,34 @@ const HomePage = () => {
                 itemClass="carousel-item-padding-40-px">
                 
                 <div className={carStyle2}>
-                    <img src={require('../assets/Pediatrician.png')}/>
+                    <GiPillDrop className='text-3xl'/>
                 </div>
                 <div className={carStyle2}>
-                    <img src={require('../assets/neuro.png')}/>
+                    <FaHeartbeat className='text-3xl'/>
                 </div>
                 <div className={carStyle2}>
-                    <img src={require('../assets/Cardiologist.png')}/>
+                    <TbPhysotherapist className='text-3xl'/>
                 </div>
                 <div className={carStyle2}>
-                    <img src={require('../assets/neuro.png')}/>
+                    <GiBrain className='text-3xl'/>
                 </div>
                 <div className={carStyle2}>
-                    <img src={require('../assets/Pediatrician.png')}/>
+                    <GiChemicalDrop className='text-3xl'/>
                 </div>
                 <div className={carStyle2}>
-                    <img src={require('../assets/neuro.png')}/>
+                    <FaTeeth className='text-3xl'/>
                 </div>
                 <div className={carStyle2}>
-                    <img src={require('../assets/Cardiologist.png')}/>
+                    <RiWomenLine className='text-3xl'/>
                 </div>
                 <div className={carStyle2}>
-                    <img src={require('../assets/neuro.png')}/>
-                </div>
-                <div className={carStyle2}>
-                    <img src={require('../assets/Pediatrician.png')}/>
-                </div>
+                    <TbBone className='text-3xl'/>
+                </div>  
             </Carousel>
 
-            <div className='py-5 px-5 flex flex-1 justify-between items-center'>
-                <div className='text-xl font-semibold'>Available Doctors</div>
-                <div>see all</div>
+            <div className='mt-5 py-5 px-5 flex flex-1 justify-between items-center'>
+                <div className='text-2xl font-semibold'>Available Doctors</div>
+                <div><Link to='/doctors'>see all</Link></div>
             </div>
 
             <Carousel
@@ -165,21 +170,110 @@ const HomePage = () => {
                 dotListClass="custom-dot-list-style"
                 itemClass="carousel-item-padding-40-px">
                 
-                <div className='px-2'>
-                    <img src={require("../assets/1.png")} className="h-56 w-max rounded-md border border-black"/>
+                <div className='h-full ml-3 border border-gray-400 rounded-sm flex flex-col items-center px-3 py-3'>
+                    <p className='py-2 font-semibold text-xl text-blue-700'>Dr. XYZ</p>
+                    <div className='w-full px-3 flex flex-1 justify-evenly items-center mt-2'>
+                        <img className='w-20 h-20 rounded-full' src={require("../assets/doc.png")}/>
+                        <div className='h-fit flex flex-col items-end mt-2'>
+                            <div className='flex flex-1 gap-2'>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                            </div>
+                            <div className='font-semibold text-lg'>Medicines</div>
+                            <div><span className='font-semibold px-1'>5</span>years Experience</div>
+                            <div className='text-lg'><span className='font-semibold px-1'>2.7K</span>Patients</div>
+                        </div>
+                    </div>
+                    <button className='px-5 py-2 w-max bg-blue-600 font-semibold text-white text-xl rounded-md mt-5'>Book Appointment Now</button>
                 </div>
-                <div>
-                    <img src={require("../assets/2.png")} className="h-56 w-max rounded-md border border-black"/>
+                
+                <div className='h-full border border-gray-400 ml-3 rounded-sm flex flex-col items-center px-3 py-3'>
+                    <p className='py-2 font-semibold text-xl text-blue-700'>Dr. XYZ</p>
+                    <div className='w-full px-3 flex flex-1 justify-evenly items-center mt-2'>
+                        <img className='w-20 h-20 rounded-full' src={require("../assets/doc.png")}/>
+                        <div className='h-fit flex flex-col items-end mt-2'>
+                            <div className='flex flex-1 gap-2'>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                            </div>
+                            <div className='font-semibold text-lg'>Medicines</div>
+                            <div><span className='font-semibold px-1'>5</span>years Experience</div>
+                            <div className='text-lg'><span className='font-semibold px-1'>2.7K</span>Patients</div>
+                        </div>
+                    </div>
+                    <button className='px-5 py-2 w-max bg-blue-600 font-semibold text-white text-xl rounded-md mt-5'>Book Appointment Now</button>
                 </div>
-                <div>
-                    <img src={require("../assets/1.png")} className="h-56 w-max rounded-md border border-black"/>
+
+                <div className='h-full border border-gray-400 ml-3 rounded-sm flex flex-col items-center px-3 py-3'>
+                    <p className='py-2 font-semibold text-xl text-blue-700'>Dr. XYZ</p>
+                    <div className='w-full px-3 flex flex-1 justify-evenly items-center mt-2'>
+                        <img className='w-20 h-20 rounded-full' src={require("../assets/doc.png")}/>
+                        <div className='h-fit flex flex-col items-end mt-2'>
+                            <div className='flex flex-1 gap-2'>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                            </div>
+                            <div className='font-semibold text-lg'>Medicines</div>
+                            <div><span className='font-semibold px-1'>5</span>years Experience</div>
+                            <div className='text-lg'><span className='font-semibold px-1'>2.7K</span>Patients</div>
+                        </div>
+                    </div>
+                    <button className='px-5 py-2 w-max bg-blue-600 font-semibold text-white text-xl rounded-md mt-5'>Book Appointment Now</button>
                 </div>
-                <div className='px-2'>
-                    <img src={require("../assets/2.png")} className="h-56 w-max rounded-md border border-black"/>
+
+                <div className='h-full border border-gray-400 ml-3 rounded-sm flex flex-col items-center px-3 py-3'>
+                    <p className='py-2 font-semibold text-xl text-blue-700'>Dr. XYZ</p>
+                    <div className='w-full px-3 flex flex-1 justify-evenly items-center mt-2'>
+                        <img className='w-20 h-20 rounded-full' src={require("../assets/doc.png")}/>
+                        <div className='h-fit flex flex-col items-end mt-2'>
+                            <div className='flex flex-1 gap-2'>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                            </div>
+                            <div className='font-semibold text-lg'>Medicines</div>
+                            <div><span className='font-semibold px-1'>5</span>years Experience</div>
+                            <div className='text-lg'><span className='font-semibold px-1'>2.7K</span>Patients</div>
+                        </div>
+                    </div>
+                    <button className='px-5 py-2 w-max bg-blue-600 font-semibold text-white text-xl rounded-md mt-5'>Book Appointment Now</button>
                 </div>
+
+                <div className='h-full border border-gray-400 ml-3 rounded-sm flex flex-col items-center px-3 py-3'>
+                    <p className='py-2 font-semibold text-xl text-blue-700'>Dr. XYZ</p>
+                    <div className='w-full px-3 flex flex-1 justify-evenly items-center mt-2'>
+                        <img className='w-20 h-20 rounded-full' src={require("../assets/doc.png")}/>
+                        <div className='h-fit flex flex-col items-end mt-2'>
+                            <div className='flex flex-1 gap-2'>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                                <span><FaStar className='text-yellow-400'/></span>
+                            </div>
+                            <div className='font-semibold text-lg'>Medicines</div>
+                            <div><span className='font-semibold px-1'>5</span>years Experience</div>
+                            <div className='text-lg'><span className='font-semibold px-1'>2.7K</span>Patients</div>
+                        </div>
+                    </div>
+                    <button className='px-5 py-2 w-max bg-blue-600 font-semibold text-white text-xl rounded-md mt-5'>Book Appointment Now</button>
+                </div>
+
             </Carousel>
         </div>
     )
 };
 
 export default HomePage;
+
