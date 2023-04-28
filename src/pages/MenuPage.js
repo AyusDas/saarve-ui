698d9cda98764d613e316769044f4ac9628f112b
AyusDas <45256705+AyusDas@users.noreply.github.com>
2023-04-28 23:22:06 +0530

    Update MenuPage.js

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,48 +1,79 @@
 import React from 'react'
 import Header from '../components/Header';
 import { Link } from "react-router-dom";
-import { BsFillBookmarksFill } from "react-icons/bs";
+import { BsFillBookmarksFill, BsPersonSquare, BsAlarm } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
 import { AiFillSetting } from "react-icons/ai";
-import { BiLogOut } from "react-icons/bi";
-import { MdCategory } from "react-icons/md";
+import { BiLogOut, BiTransferAlt, BiHelpCircle } from "react-icons/bi";
 import { RiHistoryLine } from "react-icons/ri";
+import 'react-multi-carousel/lib/styles.css';
+import boySvg from '../assets/boy-svgrepo-com.svg';
 
 const MenuPage = () => {
-    const liClass = "py-5 px-5 text-xl flex flex-1 gap-3 items-center text-blue-700 font-semibold"
+    const liClass = "py-5 px-5 text-xl flex flex-1 gap-3 items-center text-blue-600 font-semibold"
     return (
         <div>
             <Header/>
             <div>
                 <ul>
+                    <li className='flex gap-3 py-3 px-3 items-center'>
+                        <img src={boySvg} className='w-16 h-16 rounded-full' />
+                        <div className='px-3 font-semibold text-xl'>
+                            Harish Kumar<br/>
+                            <Link className='text-blue-600 font-semibold'>
+                                View Profile
+                            </Link>
+                        </div>
+                    </li>
+                    <hr/>
                     <li className={liClass}>
-                        <MdCategory />
-                        <Link>Categories</Link>
+                        <BsPersonSquare/>
+                        <Link className='text-black'>Consult A Doctor</Link>
                     </li>
                     <hr/>
                     <li className={liClass}>
                         <BsFillBookmarksFill />
-                        <Link>My Appointments</Link>
+                        <Link className='text-black'>Book A Test</Link>
                     </li>
                     <hr/>
                     <li className={liClass}>
                         <FaUserCircle />
-                        <Link>Profile</Link>
+                        <Link className='text-black'>Appointments</Link>
                     </li>
                     <hr/>
                     <li className={liClass}>
                         <AiFillSetting />
-                        <Link>Settings</Link>
+                        <Link className='text-black'>Medical Records</Link>
                     </li>
                     <hr/>
                     <li className={liClass}>
                         <RiHistoryLine/>
-                        <Link>History</Link>
+                        <Link className='text-black'>Saved Doctors</Link>
+                    </li>
+                    <hr/>
+                    <li className={liClass}>
+                        <BiTransferAlt/>
+                        <Link className='text-black'>Transactions</Link>
+                    </li>
+                    <hr/>
+                    <li className={liClass}>
+                        <BsAlarm />
+                        <Link className='text-black'>Reminders</Link>
+                    </li>
+                    <hr/>
+                    <li className={liClass}>
+                        <BiHelpCircle />
+                        <Link className='text-black'>Help</Link>
+                    </li>
+                    <hr/>
+                    <li className={liClass}>
+                        <AiFillSetting />
+                        <Link className='text-black'>Settings</Link>
                     </li>
                     <hr/>
                     <li className={liClass}>
                         <BiLogOut />
-                        <Link>Logout</Link>
+                        <Link className='text-black'>Logout</Link>
                     </li>
                 </ul>
             </div>
@@ -51,4 +82,3 @@ const MenuPage = () => {
 }
 
 export default MenuPage;
-
