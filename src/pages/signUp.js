8a8ec8fb5df287be15bf455be90ae1801d4a4477
AyusDas <45256705+AyusDas@users.noreply.github.com>
2023-05-03 22:54:08 +0530

    Update signUp.js

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -2,18 +2,19 @@ import React, { useContext } from 'react'
 import './signUp.css'
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/authContext';
+import './HomePage.css'
 
 const SignUp = () => {
     const inputClassName = "mt-2 rounded-md py-3 text-base font-normal px-3 border border-[#8888885c] w-full lg:w-1/3 focus:shadow-xl focus:border-0 focus:outline-none"
-    const formDivClassName = "text-xl text-[#6e6e6e] font-semibold tracking-wide mt-7"
+    const formDivClassName = "text-xl text-[#6e6e6e] font-semibold tracking-wide mt-7 nice-font"
     let { SignUpUser } = useContext(AuthContext)
     return (
-        <div className='mybg h-screen w-screen'>
-            <div className="absolute top-24 left-9 block w-5/6">
-                <p className="text-3xl font-semibold text-[#333333] py-3">
+        <div className=''>
+            <div className="absolute top-8 left-9 block w-5/6">
+                <p className="text-3xl font-semibold text-[#333333] py-3 nice-font">
                     Create Account
                 </p>
-                <p className="text-lg text-[#3a3a3a]">
+                <p className="text-base text-[#3a3a3a] nice-font">
                     Already have an account ? 
                     <span className="px-2 text-blue-700 font-semibold hover:underline"><Link to="/landing">Sign In !</Link></span>
                 </p>
@@ -33,9 +34,25 @@ const SignUp = () => {
                         </div>
                         <div className={formDivClassName}>
                             <p>Confirm password</p>
-                            <input className={inputClassName} type="password" />
+                            <input className={inputClassName} type="password" name="confirmPassword"/>
                         </div>
-                        <input type="submit" value="Sign In" className="mt-10 bg-blue-700 text-lg font-semibold text-white py-3 px-2 rounded-md w-full lg:w-1/3 hover:bg-blue-800"/>
+                        <div className={formDivClassName}>
+                            <p>Your Address</p>
+                            <input className='mt-2 rounded-md py-3 text-base font-normal px-3 border border-[#8888885c] w-full h-32 lg:w-1/3 focus:shadow-xl focus:border-0 focus:outline-none' type="text" name="address"/>
+                        </div>
+                        <div className={formDivClassName}>
+                            <p>City</p>
+                            <input className={inputClassName} type="text" name="City"/>
+                        </div>
+                        <div className={formDivClassName}>
+                            <p>Gender</p>
+                            <input className={inputClassName} type="text" name="gender"/>
+                        </div>
+                        <div className={formDivClassName}>
+                            <p>Date Of Birth</p>
+                            <input className={inputClassName} type="date" name="dateOfBirth"/>
+                        </div>
+                        <input type="submit" value="Save" className="mt-10 bg-blue-700 text-lg font-semibold nice-font text-white py-3 px-2 rounded-md w-full lg:w-1/3 hover:bg-blue-800"/>
                     </form>
                 </div>
             </div>
@@ -44,3 +61,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
