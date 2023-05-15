53803f07d9de156f184c138deda3eb847f1f66b3
AyusDas <45256705+AyusDas@users.noreply.github.com>
2023-05-15 21:03:07 +0530

    Update search.js

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BsArrowLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import './HomePage.css';
 
 const Search = () => {
   return (
@@ -9,29 +10,29 @@ const Search = () => {
                 <div className="text-3xl px-2">
                     <Link to="/home"><BsArrowLeft/></Link>
                 </div>
-                <div className='text-3xl font-semibold w-5/6 text-center'>
+                <div className='text-3xl font-semibold absolute right-1/2 translate-x-1/2 nice-font'>
                     Search
                 </div>
             </header>
             <div className='flex flex-1 justify-between items-center bg-[#E6EFF9] mt-2'>
                 <div className='px-5 py-5'>
-                    <div className='text-xl'>Search Your</div>
-                    <div className='text-2xl font-semibold'>Specialist</div>
+                    <div className='text-xl nice-font'>Search Your</div>
+                    <div className='text-2xl font-semibold nice-font'>Specialist</div>
                 </div>
             </div>
             <div className='w-full py-5 bg-[#E6EFF9]'>
                 <form className='w-full flex flex-col items-center'>
                     <div className="w-full px-10 py-5">
-                        <input name="area" className='w-full px-5 py-3 rounded-md outline-none shadow-lg' placeholder="Area/location"/>
+                        <input name="area" className='w-full px-5 py-3 rounded-md outline-none shadow-lg nice-font' placeholder="Area/location"/>
                     </div>
                     <div className="w-full px-10 py-5">
-                        <input name="speciality" className='w-full px-5 py-3 rounded-md outline-none shadow-lg' placeholder="Doctor/Speciality"/>
+                        <input name="speciality" className='w-full px-5 py-3 rounded-md outline-none shadow-lg nice-font' placeholder="Doctor/Speciality"/>
                     </div>
                     <div className="w-full px-10 py-5">
-                        <input type="date" name="date" className='w-full px-5 py-3 rounded-md outline-none shadow-lg'/>
+                        <input type="date" name="date" className='w-full px-5 py-3 rounded-md outline-none shadow-lg nice-font'/>
                     </div>
                     <div className="w-full px-10 py-5">
-                        <input type="button" value="Search" className='w-full px-5 py-3 rounded-md outline-none shadow-lg bg-blue-600 text-white text-xl font-semibold'/>
+                        <input type="button" value="Search" className='w-full px-5 py-3 rounded-md outline-none shadow-lg bg-blue-600 text-white text-xl font-semibold nice-font'/>
                     </div>
                 </form>
             </div>
@@ -43,4 +44,3 @@ const Search = () => {
 }
 
 export default Search;
-
